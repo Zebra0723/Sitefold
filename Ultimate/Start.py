ccdfd1fe778fb751cf305d2b4f3cda3b3ea879bc
Zebra0723 <arjunvirjain@icloud.com>
2025-05-02 22:15:54 +0100

    Update Start.py

--- Ultimate/Start.py
+++ Ultimate/Start.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="SONGARENA TEST")
+st.set_page_config(
+    page_title="SONGARENA START",
+    layout="wide",
+    initial_sidebar_state="expanded"  # 🔥 this line is the magic
+)
 
 st.title("🚀 SongArena Start Page")
 st.caption("Use the sidebar to navigate manually.")
