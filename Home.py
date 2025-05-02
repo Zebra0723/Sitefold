c2b7feea7cf9cfe2997aac869d95e3561b161229
Zebra0723 <arjunvirjain@icloud.com>
2025-05-02 21:46:27 +0100

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="SONGARENA TEST", layout="centered")
+
+st.title("🔥 Jump Into the Arena")
+st.page_link("pages/Test.py", label="✅ Open Test Page")
