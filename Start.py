7587de96e2d1ac2d0a692c40634eea8482ba5c6a
Zebra0723 <arjunvirjain@icloud.com>
2025-05-02 22:50:10 +0100

    Create Start.py

---
+++ Start.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="SONGARENA ULTIMATE",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("🎵 SONGARENA ULTIMATE")
+st.write("👈 Use the sidebar on the left to navigate.")
