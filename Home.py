6f6c57cdb5787c918def4b6e9ceba5a30b32fce1
Zebra0723 <arjunvirjain@icloud.com>
2025-05-02 21:50:59 +0100

    Delete Home.py

--- Home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="SONGARENA TEST", layout="centered")
-
-st.title("🔥 Jump Into the Arena")
-st.page_link("pages/Test.py", label="✅ Open Test Page")
