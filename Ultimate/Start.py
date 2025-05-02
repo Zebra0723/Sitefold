25d29d179e3c2d8fad3a2507feec96a35d10794e
Zebra0723 <arjunvirjain@icloud.com>
2025-05-02 22:13:14 +0100

    Update Start.py

--- Ultimate/Start.py
+++ Ultimate/Start.py
@@ -1,24 +1,8 @@
 import streamlit as st
 
-st.set_page_config(page_title="SONGARENA ULTIMATE", layout="wide")
+st.set_page_config(page_title="SONGARENA TEST")
 
-st.sidebar.title("🎵 SONGARENA MENU")
-page = st.sidebar.radio(
-    "Go to",
-    ["Home", "Battle", "Hall", "Add Songs", "Song Stats", "Reset"]
-)
+st.title("🚀 SongArena Start Page")
+st.caption("Use the sidebar to navigate manually.")
 
-if page == "Home":
-    st.title("🎵 SONGARENA ULTIMATE")
-    st.caption("Only one track will survive. But many will fight.")
-    st.markdown("Use the sidebar to navigate.")
-elif page == "Battle":
-    exec(open("pages/Battle.py").read())
-elif page == "Hall":
-    exec(open("pages/Hall.py").read())
-elif page == "Add Songs":
-    exec(open("pages/AddSong.py").read())
-elif page == "Song Stats":
-    exec(open("pages/SongStats.py").read())
-elif page == "Reset":
-    exec(open("pages/Reset.py").read())
+st.markdown("Go to the top-left menu ➜ select ‘Battle’ to launch the arena.")
